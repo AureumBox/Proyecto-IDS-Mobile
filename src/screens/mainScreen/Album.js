aca0c5acb6c69b91b9747dbf4427338454d01ba3
Cristina Morales <cristimoralespadron@icloud.com>
2022-11-26 01:17:49 -0400

    La pantalla principal de album mas arrecha que veran en su vida <3

--- src/screens/mainScreen/Album.js
+++ src/screens/mainScreen/Album.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import * as Font from 'expo-font';
 import {
     StyleSheet,
     Text,
     View,
-    Dimensions
+    Dimensions,
+    Image
 } from "react-native";
 
 import Header from '../../components/HeaderComponent';
+import AlbumDigital from '../../../assets/albumd.png';
 
 export default function Album({ navigation }) {
     const [fontsLoaded, setFontsLoaded] = useState(false)
@@ -33,9 +35,18 @@ export default function Album({ navigation }) {
         <View style={styles.fondo}>
         <Header/>
         <View style={styles.container}>
-            <Text style={styles.textSt}>
-                Album
-            </Text>
+        <View style={styles.barraPorcentaje}>
+        <Text style={styles.texto}>30%</Text>
+        <View style={styles.Porcentaje}></View>    
+        </View>
+            <View style={styles.rectangulo}>
+                <Image
+                source={AlbumDigital}
+                style={styles.albumdig}></Image>
+            </View>
+            <View style={styles.carrusel}>
+            <Text style={styles.textSt}>¡Pega tus cromos!</Text>    
+            </View> 
         </View>
         </View>
     )
@@ -49,13 +60,62 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: '#70ABAF',
     },
+    albumdig:{
+        resizeMode: 'contain',
+        width: 420,
+        marginLeft: 10
+    },
+    rectangulo: {
+        width: 350,
+        height: 460,
+        backgroundColor: 'white',
+        marginBottom: 30,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    texto:{
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 26,
+        marginLeft: -63, 
+    },
+    barraPorcentaje:{
+        width: 250,
+        height: 40,
+        backgroundColor: 'white',
+        borderRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 25,
+        flexDirection: 'row',
+        marginLeft: 55
+    },
+    Porcentaje:{
+        width: 80,
+        height: 40,
+        backgroundColor: '#63130B',
+        borderRadius: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 170,
+        marginLeft: 5,
+    },
+    carrusel: {
+        width: 350,
+        height: 65,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     fondo: {
         flex: 1,
         backgroundColor: '#70ABAF',
     },
     textSt: {
         fontWeight: 'bold',
-        color: '#C10001',
+        color: '#34545D',
         fontSize: 26,
         // fontFamily: 'averta-demo-pe',
     },
