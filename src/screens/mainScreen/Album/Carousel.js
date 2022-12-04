891fbc0012daa0495f794950d7861da83952cd3d
Aurimart Garcia <aurimart.liliana@gmail.com>
2022-12-04 10:27:15 -0400

    terminando carrusel de stickers (front)

--- src/screens/mainScreen/Album/Carousel.js
+++ src/screens/mainScreen/Album/Carousel.js
@@ -6,17 +6,15 @@ import {
 const { width, height } = Dimensions.get('screen');
 const data = [
     'https://img.freepik.com/foto-gratis/gato-rojo-o-blanco-i-estudioblanco_155003-13189.jpg?w=2000',
+    'https://img.freepik.com/foto-gratis/gato-rojo-o-blanco-i-estudioblanco_155003-13189.jpg?w=2000',
+    'https://img.freepik.com/foto-gratis/gato-rojo-o-blanco-i-estudioblanco_155003-13189.jpg?w=2000',
+    'https://img.freepik.com/foto-gratis/gato-rojo-o-blanco-i-estudioblanco_155003-13189.jpg?w=2000',
+    'https://img.freepik.com/foto-gratis/gato-rojo-o-blanco-i-estudioblanco_155003-13189.jpg?w=2000',
+    'https://img.freepik.com/foto-gratis/gato-rojo-o-blanco-i-estudioblanco_155003-13189.jpg?w=2000',
     'https://okdiario.com/img/2022/05/12/gato-655x368.jpg',
-    'https://encryptedtbn0.gstatic.com/images?q=tbn:ANd9GcS8t5Qv9hoArKjwgA25zZgoNoKuhbVU2zc6-A & usqp=CAU',
-    'https://www.zooplus.es/magazine/wp-content/uploads/2022/05/Cuanto-pesa-ungato-2.jpeg',
-    'https://tucomunidad.com.pa/wp-content/uploads/2021/10/GATO.jpg',
-    'https://petpillow.com.br/wp-content/uploads/2019/11/como-saber-se-o-gatoesta-com-dor.jpg',
-    'https://styleandleisurenews.com/wp-content/uploads/2021/08/gatos-gestosm.jpeg'
 ];
-const imageW = width * 0.7;
-const imageH = imageW * 1.54;
 
-export default function Carousel(){
+export default function Carousel() {
     return (
         <View style={styles.carousel}>
             <View>
@@ -25,16 +23,28 @@ export default function Carousel(){
                     keyExtractor={(_, index) => index.toString()}
                     horizontal
                     pagingEnabled
+                    ListEmptyComponent={
+                        () => <View style={{ justifyContent: 'center' }}>
+                            <Text style={{ color: 'gray' }}>No hay cromos disponibles para pegar</Text>
+                        </View>
+                    }
+                    ListFooterComponent={
+                        () => <View style={{ width: 20}} />
+                    }
+                    ListHeaderComponent={
+                        () => <View style={{ width: 20}} />
+                    }
                     ItemSeparatorComponent={
-                        () => <View style={{ width: 20 }} />
+                        () => <View style={{ width: 35}} />
                     }
                     renderItem={({ item }) => {
-                        return <View>
-                            <Image source={{ uri: item }} style={{
-                                width: imageW,
-                                height: imageH,
-                                resizeMode: 'cover',
-                            }} />
+                        return <View >
+                            <View style={{ width: 150, height: '95%',  justifyContent: 'center' }}>
+                                <Image source={{ uri: item }} style={{
+                                    flex: 1,
+                                    resizeMode: 'contain',
+                                }} />
+                            </View>
                         </View>
                     }}
                 />
