a7cca78fddfd04276ab991ee7368b134c4b2a847
Cristina Morales <cristimoralespadron@icloud.com>
2022-12-26 16:17:55 -0400

    HomePage en proceso

--- src/screens/mainScreen/Home.js
+++ src/screens/mainScreen/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 import {
     StyleSheet,
     View,
-    Text
+    Text,
+    Image
 } from "react-native";
 import Header from '../../components/HeaderComponent';
+import Sobre from '../../../assets/app/sobre.png';
 
 export default function Home({ navigation }) {
     return (
@@ -12,8 +14,14 @@ export default function Home({ navigation }) {
             <Header />
             <View style={styles.container}>
                 <Text style={styles.textSt}>
-                    Home
+                    Bienvenido usuario
                 </Text>
+                <View style={styles.sobreContainer}>
+                <Image source={Sobre} style={styles.sobreImg}></Image>
+                <View style={styles.botonReclamar}>
+                    <Text style={styles.textReclamar}>Reclamar</Text>
+                </View>    
+                </View>    
             </View>
         </View>
     )
@@ -23,16 +31,43 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
         backgroundColor: '#70ABAF',
+    },
+    sobreImg:{
+        height: '75%',
+        resizeMode: 'contain'
+    },
+    sobreContainer:{
+        backgroundColor: 'white',
+        width: '55%',
+        height: '40%',
+        borderRadius: 10,
+        justifyContent: 'space-evenly',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    botonReclamar:{
+        backgroundColor: '#70ABAF',
+        borderRadius: 20,
+        height: '15%',
+        width: '60%', 
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     fondo: {
         flex: 1,
         backgroundColor: '#70ABAF',
     },
     textSt: {
-        color: '#C10001',
+        color: 'white',
         fontWeight: 'bold',
         fontSize: 26,
     },
+    textReclamar: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+
 })
