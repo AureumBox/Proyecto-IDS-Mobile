340c06ea72ee0aa665c199a1246ed0b157b13977
ajrosas <alejandrojrosas2001@gmail.com>
2023-01-10 13:47:56 -0400

    easter egg :)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  StyleSheet,
-  View,
-  Image,
-  Dimensions,
-  Text
+	StyleSheet,
+	View,
+	Image,
+	Dimensions,
+	Text,
+	TouchableOpacity
 } from 'react-native';
 import { useSelector } from "react-redux";
 import Icon from 'react-native-remix-icon';
+import { Audio } from 'expo-av'
 
 import logoImg from '../../assets/app/logoHorizontal.png';
 
 const { width } = Dimensions.get('window');
 
 export default function HeaderComponent() {
-  const { money } = useSelector((state) => state.auth);
-  return (
-    <View style={styles.header}>
-      <Image source={logoImg} style={styles.logo} />
-      <View style={styles.coins}>
-        <Icon name="money-dollar-circle-fill" size="30" color="#E7484D" />
-        <Text style={styles.coinsText}>{money}</Text>
-      </View>
-    </View>
-  );
+	const { money } = useSelector((state) => state.auth);
+
+	// Easter Egg
+	const [count, setCount] = useState(0);
+	const [sound, setSound] = useState();
+
+	async function playSound() {
+		console.log('Loading Sound');
+		const { sound } = await Audio.Sound.createAsync(require('../../assets/app/easter-egg.mp3'));
+		setSound(sound);
+
+		console.log('Playing Sound');
+		await sound.playAsync();
+	}
+
+	useEffect(() => {
+		return sound
+			? () => {
+				console.log('Unloading Sound');
+				sound.unloadAsync();
+			}
+			: undefined;
+	}, [sound]);
+	//******************************************************/
+
+	return (
+		<View style={styles.header}>
+			<TouchableOpacity
+				onPress={() => {
+					setCount(count + 1)
+					if(count == 10){
+						setCount(0)
+						playSound()
+					}
+				}}
+			>
+				<Image source={logoImg} style={styles.logo} />
+			</TouchableOpacity>
+			<View style={styles.coins}>
+				<Icon name="money-dollar-circle-fill" size="30" color="#E7484D" />
+				<Text style={styles.coinsText}>{money}</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  header: {
-    width: width,
-    backgroundColor: 'white',
-    alignSelf: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  logo: {
-    width: 125,
-    resizeMode: 'contain',
-    left: width / 32
-  },
-  coins: {
-    width: 80,
-    height: 30,
-    justifyContent: 'center',
-    backgroundColor: '#D9D9D9',
-    borderRadius: 18,
-    right: width / 16
-  },
-  coinsText: {
-    position: 'absolute',
-    left: '40%',
-    fontSize: 20,
-    fontWeight: '600'
-  },
+	header: {
+		width: width,
+		backgroundColor: 'white',
+		alignSelf: 'center',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	logo: {
+		width: 125,
+		resizeMode: 'contain',
+		left: width / 32
+	},
+	coins: {
+		width: 80,
+		height: 30,
+		justifyContent: 'center',
+		backgroundColor: '#D9D9D9',
+		borderRadius: 18,
+		right: width / 16
+	},
+	coinsText: {
+		position: 'absolute',
+		left: '40%',
+		fontSize: 20,
+		fontWeight: '600'
+	},
 });
