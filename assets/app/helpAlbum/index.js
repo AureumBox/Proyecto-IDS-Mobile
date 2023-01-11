2b109516e7c5f5c201085f0a5e58dfeb698cc85f
Alejandro <alejandrojrosas2001@gmail.com>
2023-01-11 10:25:13 -0400

    Modal advances

---
+++ assets/app/helpAlbum/index.js
@@ -0,0 +1,22 @@
+export default [
+	{
+		id: '1',
+		description: 'Para pegar cromos en el álbum selecciona el cromo que desees pegar del carrusel',
+		image: require('./h1.png')
+	},
+	{
+		id: '2',
+		description: 'Pulsa el espacio en el álbum correspondiente al nombre del cromo para pegarlo',
+		image: require('./h2.png')
+	},
+	{
+		id: '3',
+		description: 'Puedes acceder al filtro de equipo fácilmente al tocar el nombre del equipo',
+		image: require('./h3.png')
+	},
+    {
+		id: '4',
+		description: 'Para cambiar de equipo rápidamente en el álbum toca el equipo que deseas ver en el filtro',
+		image: require('./h4.png')
+	}
+]
--- src/screens/homePageAuth/landingPage/SliderContent.js
+++ src/screens/homePageAuth/landingPage/SliderContent.js
@@ -3,18 +3,18 @@ export default [
 		id: '1',
 		title: '¡Bienvenido a Offside!',
 		description: 'Organiza, colecciona y compite en una liga llena de amigos con tu equipo de ensueño favorito',
-		image: require('../../../../assets/splash.png')
+		image: require('../../../../assets/app/landingPage/lp1.png')
 	},
 	{
 		id: '2',
 		title: 'Llena tu álbum',
 		description: 'Colecciona todos los cromos y llena tu álbum con tus jugadores favoritos',
-		image: require('../../../../assets/app/LlenarAlbum.png')
+		image: require('../../../../assets/app/landingPage/lp2.png')
 	},
 	{
 		id: '3',
 		title: 'Participa en el fantasy',
 		description: 'Crea tu equipo de ensueño y participa en una liga para ganar puntos',
-		image: require('../../../../assets/app/Participafantasy.png')
+		image: require('../../../../assets/app/landingPage/lp3.png')
 	},
 ]
